629733f8ccb7c51d1472d25eec8fb244edb7e610
MariemBH <mariem.benhassen@ensi-uma.tn>
2026-02-22 01:15:20 +0100

    handle google login

--- components/AuthButton.jsx
+++ components/AuthButton.jsx
@@ -1,11 +1,21 @@
 "use client";
 import React, { useState } from "react";
 import { Button } from "./ui/button";
-import { LogIn } from "lucide-react";
-import {AuthModal} from "./AuthModal"
+import { LogIn, LogOut } from "lucide-react";
+import { AuthModal } from "./AuthModal";
 
-const AuthButton = ({user}) => {
-    const [showAuthModal, setShowAuthModal] = useState(false);
+const AuthButton = ({ user }) => {
+  const [showAuthModal, setShowAuthModal] = useState(false);
+  if (user) {
+    return (
+      <form action={signOut}>
+        <Button variant="ghost" size="sm" type="submit" className="gap-2">
+          <LogOut className="w-4 h-4" />
+          Sign Out
+        </Button>
+      </form>
+    );
+  }
   return (
     <>
       <Button
@@ -18,9 +28,9 @@ const AuthButton = ({user}) => {
         Sign In
       </Button>
       <AuthModal
-       isOpen={showAuthModal} 
-       onClose={()=>setShowAuthModal(false)}
-       />
+        isOpen={showAuthModal}
+        onClose={() => setShowAuthModal(false)}
+      />
     </>
   );
 };
